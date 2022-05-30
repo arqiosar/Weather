601516652989b920b59ead0967539d2d3c4ddb50
Sara Arqioui <sara1arqioui@gmail.com>
2022-05-30 17:16:44 +0200

    CurrentLocationWeather call

--- app/src/main/java/fr/mastergime/arqioui/weather/fragments/LocationWeatherFragment.kt
+++ app/src/main/java/fr/mastergime/arqioui/weather/fragments/LocationWeatherFragment.kt
@@ -1,32 +1,44 @@
 package fr.mastergime.arqioui.weather.fragments
 
-import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
+import android.content.Context
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.lifecycle.Observer
 import fr.mastergime.arqioui.weather.databinding.FragmentLocationWeatherBinding
 import fr.mastergime.arqioui.weather.models.WeatherViewModel
+import fr.mastergime.arqioui.weather.util.Constants
+import android.Manifest
+import androidx.fragment.app.viewModels
+import fr.mastergime.arqioui.weather.factory.LocationWeatherFactory
+import fr.mastergime.arqioui.weather.factory.SearchCityWeatherFactory
+import fr.mastergime.arqioui.weather.models.SearchWeatherByCityViewModel
+import fr.mastergime.arqioui.weather.repository.RequestRepository
 
+class LocationWeatherFragment : Fragment(), LocationListener {
 
-class LocationWeatherFragment : Fragment() {
+    private lateinit var locationManager: LocationManager
+    private val locationPermissionCode = 2
 
-    private val REQUEST_CODE = 1
-
-    private lateinit var viewModel: WeatherViewModel
+    private val LWViewModel: WeatherViewModel by viewModels(factoryProducer = {
+        LocationWeatherFactory(
+            RequestRepository()
+        )
+    })
     private lateinit var _LWBinding: FragmentLocationWeatherBinding
 
-    lateinit var mLocationManager: LocationManager
-    lateinit var mLocationListener: LocationListener
-
-    //var location: SimpleLocation? = null
-    var latitude: String? = null
-    var longitude: String? = null
+    private var latitude: String? = null
+    private var longitude: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,99 +51,102 @@ class LocationWeatherFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        getLocation()
+
+        LWViewModel.getWeatherDataWithGPS(latitude!!, longitude!!, Constants.METRIC)
+
+        LWViewModel.locationData.observe(viewLifecycleOwner, Observer { locationGps ->
+            locationGps?.let {
+                _LWBinding.lytLocation.visibility = View.VISIBLE
+                //_LWBinding.locationGPS = locationGps
+                _LWBinding.tvTemperature.text = locationGps.main!!.temp.toInt().toString()
+                //_LWBinding.tvDate.text = dateConverter()
+                //_LWBinding.tvSunrise.text = timeConverter((locationGps.sys!!.sunrise).toLong())
+                //_LWBinding.tvSunset.text = timeConverter((locationGps.sys!!.sunset).toLong())
+                _LWBinding.imgState.setImageResource(resources.getIdentifier("ic_"+locationGps.weather?.get(0)?.icon, "drawable", view.context.packageName))
+
+            }
+        })
+
+        LWViewModel.locationLoading.observe(viewLifecycleOwner, Observer { loading ->
+            loading?.let {
+                if (it){
+                    //locationLoading.visibility = View.VISIBLE
+                    //lytLocation.visibility = View.GONE
+                }else{
+                    //locationLoading.visibility = View.GONE
+                }
+            }
+        })
     }
-}
-/*viewModel = ViewModelProviders.of(this).get(LocationViewModel::class.java)
 
-location = SimpleLocation(context)
-if (!location!!.hasLocationEnabled()) {
-    SimpleLocation.openSettings(context)
-} else {
-    if (ContextCompat.checkSelfPermission(
-            requireActivity(),
-            android.Manifest.permission.ACCESS_FINE_LOCATION
-        ) != PackageManager.PERMISSION_GRANTED
-    ) {
-        ActivityCompat.requestPermissions(
-            requireActivity(),
-            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-            REQUEST_CODE
-        )
-    } else {
-        location = SimpleLocation(context)
-        latitude = String.format("%.6f", location?.latitude)
-        longitude = String.format("%.6f", location?.longitude)
-        Log.e("LAT1", "" + latitude)
-        Log.e("LONG1", "" + longitude)
-
+    private fun getLocation() {
+        locationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((this.context?.let {
+                ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION)
+        } != PackageManager.PERMISSION_GRANTED)) {
+            this.activity?.let {
+                ActivityCompat.requestPermissions(it, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+            }
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
     }
-}
-viewModel.getWeatherDataWithGPS(latitude!!, longitude!!, Constant.METRIC)
-
-viewModel.locationData.observe(viewLifecycleOwner, Observer { locationGps ->
-    locationGps?.let {
-        _LWBinding.lytLocation.visibility = View.VISIBLE
-        _LWBinding.locationGPS = locationGps
-        _LWBinding.tvTemperature.text = locationGps.main!!.temp.toInt().toString()
-        _LWBinding.tvDate.text = dateConverter()
-        _LWBinding.tvSunrise.text = timeConverter((locationGps.sys!!.sunrise).toLong())
-        _LWBinding.tvSunset.text = timeConverter((locationGps.sys!!.sunset).toLong())
-        _LWBinding.imgState.setImageResource(resources.getIdentifier("ic_"+locationGps.weather?.get(0)?.icon, "drawable", view.context.packageName))
-
-    }
-})
-
-/*viewModel.locationLoading.observe(viewLifecycleOwner, Observer { loading ->
-    loading?.let {
-        if (it){
-            locationLoading.visibility = View.VISIBLE
-            lytLocation.visibility = View.GONE
-        }else{
-            locationLoading.visibility = View.GONE       }
-    }
-})*/
-}
-
-/*override fun onRequestPermissionsResult(
-requestCode: Int,
-permissions: Array<out String>,
-grantResults: IntArray
-) {
-super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-if (requestCode == REQUEST_CODE) {
-    if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-        //Toast.makeText(this, "Location get Successfully", Toast.LENGTH_SHORT).show()
-        getWeatherForCurrentLocation()
-    } else {
-        //user denied the permission
-    }
-}
-}
-
-private fun getWeatherForCurrentLocation() {
-mLocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-mLocationListener = object : LocationListener {
     override fun onLocationChanged(location: Location) {
-        val latitude = location.latitude.toString()
-        val logitude = location.longitude.toString()
-        val params: RequestParams = RequestParams();
-        params.put("lat", latitude)
-        params.put("lon", logitude)
-        params.put("appid", app_id)
-        letsDoSomeNetworking(params)
+        latitude = location.latitude.toString()
+        longitude = location.longitude.toString()
     }
-
-    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this.context, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this.context, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
-
-    override fun onProviderDisabled(provider: String) {
-
-    }
-
-    override fun onProviderEnabled(provider: String) {
-
-    }
-}*/
 }
 
- */
+    /*override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Toast.makeText(this, "Location get Successfully", Toast.LENGTH_SHORT).show()
+                getWeatherForCurrentLocation()
+            } else {
+                //user denied the permission
+            }
+        }
+    }
+
+    private fun getWeatherForCurrentLocation() {
+        mLocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        mLocationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                val latitude = location.latitude.toString()
+                val logitude = location.longitude.toString()
+                val params: RequestParams = RequestParams();
+                params.put("lat", latitude)
+                params.put("lon", logitude)
+                params.put("appid", app_id)
+                letsDoSomeNetworking(params)
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+            }
+
+            override fun onProviderDisabled(provider: String) {
+
+            }
+
+            override fun onProviderEnabled(provider: String) {
+
+            }
+    }*/
+
